b265dd90e0dcbc01dc236de8e3bb5c4e54a236e7
Jaylaelike <smarkwisai@gmail.com>
2023-12-27 17:13:41 +0700

    update TableStatus url.

--- app/components/userAcountNav.tsx
+++ app/components/userAcountNav.tsx
@@ -3,11 +3,13 @@ import { Button } from '../../app/components/ui/button';
 import { signOut } from 'next-auth/react';
 import React from 'react'
 
+
 function UserAcountNav() {
+
   return (
     <>
      <Button onClick={()=> signOut({
-        callbackUrl: `${window.location.origin}/sign-in`,
+        callbackUrl:  process.env.NEXT_PUBLIC_URL + '/sign-in',
         redirect: true,
      })} variant='destructive'>Sign Out</Button>
     </>
