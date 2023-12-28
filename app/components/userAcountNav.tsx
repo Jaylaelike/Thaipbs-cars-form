b798034f1a9929b817196f12218ba49fb9f2f89a
Jaylaelike <smarkwisai@gmail.com>
2023-12-28 09:21:10 +0700

    update CallBackURL UserAcountNav.

--- app/components/userAcountNav.tsx
+++ app/components/userAcountNav.tsx
@@ -1,20 +1,24 @@
 "use client";
-import { Button } from '../../app/components/ui/button';
-import { signOut } from 'next-auth/react';
-import React from 'react'
-
+import { Button } from "../../app/components/ui/button";
+import { signOut } from "next-auth/react";
+import React from "react";
 
 function UserAcountNav() {
-
   return (
     <>
-     <Button onClick={()=> signOut({
-        callbackUrl: 'http://172.16.202.63:3000/sign-in',
-        redirect: true,
-     })} variant='destructive'>Sign Out</Button>
+      <Button
+        onClick={() =>
+          signOut({
+            callbackUrl: `${window.location.origin}/sign-in`,
+            redirect: true,
+          })
+        }
+        variant="destructive"
+      >
+        Sign Out
+      </Button>
     </>
-   
-  )
+  );
 }
 
-export default UserAcountNav
+export default UserAcountNav;
